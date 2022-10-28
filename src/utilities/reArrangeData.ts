00c57b8820461057153edd0d34f2564946a73ffd
dev-mkr <webdev.mu@gamil.com>
2022-10-28 02:12:09 +0200

    refactor raArrangeData helper fun with immerjs 😍

--- src/utilities/reArrangeData.ts
+++ src/utilities/reArrangeData.ts
@@ -1,33 +1,56 @@
 import { analysisDataType } from "../types/storeTypes";
+import { enableMapSet } from "immer";
+import produce from "immer";
+enableMapSet();
 
 import { reArrangedAnalysisDataType } from "../types/storeTypes";
 
 const reArrangeData = (data: analysisDataType) => {
-  const sortedData = data.reduce(function (mappedData, item) {
-    if (!mappedData.get(item.country) || !mappedData.get(item.country)[item.camp]) {
-      mappedData.set(item.country, { ...mappedData.get(item.country), [item.camp]: {} });
-    }
-    if (!mappedData.get(item.country)[item.camp][item.school]) {
-      mappedData.set(item.country, {
-        ...mappedData.get(item.country),
-        [item.camp]: {
-          ...mappedData.get(item.country)[item.camp],
-          [item.school]: [],
-        },
-      });
-    }
+  const sortedData = produce(new Map(), (draft) => {
+    return data.reduce((mappedData, item) => {
+      if (!mappedData.get(item.country) || !mappedData.get(item.country)[item.camp]) {
+        mappedData.set(item.country, {
+          ...mappedData.get(item.country),
+          [item.camp]: {},
+        });
+      }
+      if (!mappedData.get(item.country)[item.camp][item.school]) {
+        mappedData.get(item.country)[item.camp][item.school] = [];
+      }
+      mappedData.get(item.country)[item.camp][item.school].push(item);
+      return mappedData;
+    }, draft);
+  });
 
-    mappedData.set(item.country, {
-      ...mappedData.get(item.country),
-      [item.camp]: {
-        ...mappedData.get(item.country)[item.camp],
-        [item.school]: [...mappedData.get(item.country)[item.camp][item.school], item],
-      },
-    });
-
-    return mappedData;
-  }, new Map());
   return sortedData;
 };
+
+// const reArrangeData = (data: analysisDataType) => {
+//   const sortedData = data.reduce((mappedData, item) => {
+//     if (!mappedData.get(item.country) || !mappedData.get(item.country)[item.camp]) {
+//       mappedData.set(item.country, { ...mappedData.get(item.country), [item.camp]: {} });
+//     }
+//     if (!mappedData.get(item.country)[item.camp][item.school]) {
+//       mappedData.set(item.country, {
+//         ...mappedData.get(item.country),
+//         [item.camp]: {
+//           ...mappedData.get(item.country)[item.camp],
+//           [item.school]: [],
+//         },
+//       });
+//     }
+
+//     mappedData.set(item.country, {
+//       ...mappedData.get(item.country),
+//       [item.camp]: {
+//         ...mappedData.get(item.country)[item.camp],
+//         [item.school]: [...mappedData.get(item.country)[item.camp][item.school], item],
+//       },
+//     });
+
+//     return mappedData;
+//   }, new Map());
+//   return sortedData;
+// };
 
 export default reArrangeData;
