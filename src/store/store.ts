2a4d7a331ebf0eac181dcf882e0dd26dd217269b
dev-mkr <webdev.mu@gamil.com>
2022-10-28 18:35:49 +0200

    moved types to separate  file

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,23 @@
 import create from "zustand";
-import { analysisDataType, reArrangedAnalysisDataType } from "../types/storeTypes";
+import { StoreType } from "../types/storeTypes";
 import reArrangeData from "../utilities/reArrangeData";
-type Store = {
-  analysisData: Map<string, reArrangedAnalysisDataType> | undefined;
-  error: any;
-  fetchAnalysisData: () => void;
-  setStore: <T>(key: string, payload: T) => void;
-};
 
-const useStore = create<Store>()((set) => ({
+const useStore = create<StoreType>()((set) => ({
   analysisData: undefined,
   error: false,
-
   fetchAnalysisData: () => {
     fetch(
       "https://raw.githubusercontent.com/abdelrhman-arnos/analysis-fe-challenge/master/data.json"
     )
       .then((res) => res.json())
-      .then((data) => set({ analysisData: reArrangeData(data) }))
+      .then((data) =>
+        set({ analysisData: reArrangeData(data), selectedCountry: data[0].country })
+      )
       .catch((error) => set({ error: error }));
   },
+  selectedCountry: "",
+  selectedCamp: "",
+  selectedSchool: "",
   setStore: (key, payload) => set({ [key]: payload }),
 }));
 
